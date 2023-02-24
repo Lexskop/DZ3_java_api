276baa2599e04f5f972b6a56750f666c9e537188
lexskop <lexskop@gmail.com>
2023-02-24 19:38:30 +0700

    Две задачки решены

--- task2.java
+++ task2.java
@@ -1,8 +1,22 @@
+
 /*
 Пусть дан произвольный список целых чисел, удалить из него четные числа
  */
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class task2 {
     public static void main(String[] args) {
-        
+        ArrayList<Integer> nums = new ArrayList<Integer>(
+                Arrays.asList(2, 3, 3, 4, 9, 7, 2, 3));
+        System.out.print(nums);
+        System.out.println();
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) % 2 == 1) {
+                nums.remove(i);
+                i -= 1;
+            }
+        }
+        System.out.print(nums);
     }
 }
--- task3.java
+++ task3.java
@@ -1,8 +1,25 @@
 /*
 Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее из этого списка.
  */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class task3 {
     public static void main(String[] args) {
-        
+        ArrayList<Integer> nums = new ArrayList<Integer>(
+                Arrays.asList(1, 6, 5, 4, 9, 7, 2, 3, 10));
+        double sum = 0;
+        System.out.print(nums);
+        System.out.println();
+        Collections.sort(nums);
+        System.out.printf("Минимальное значение: %s\nСреднее значение: %s\nМаксимальное значение: %s\n", nums.get(0),
+                nums.get(nums.size() / 2), nums.get(nums.size() - 1));
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums.get(i);
+        }
+        System.out.printf("Среднее-арифметическое: %.2f", sum / nums.size()); // Просто я не понял вопрос - среднее или
+                                                                              // среднее-арифметическое в массиве
     }
 }
